6c552b50b65adc126efafdfd30b7273e232c5188
hluuy <dudwns1289@gmail.com>
2023-09-12 23:03:08 +0900

    update

--- django/23_09_12/실습/1-1/fruits/views.py
+++ django/23_09_12/실습/1-1/fruits/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+context = {
+    'fruit_list' : ["귤","딸기","사과","감","바나나","파인애플","구아바", "복숭아", "망고스틴"],
+    'hate' : ["사과","구아바"]
+}
 # Create your views here.
+def index(request):
+    return render(request, 'fruits/index.html', context)
---
+++ django/23_09_12/실습/1-6/urls.py
@@ -0,0 +1,8 @@
+# 1. 현재 폴더에서 django 프로젝트(my_pjt)와 앱(my_app)를 만들고 서버를 실행하기 위한 bash 명령어를 주석으로 작성하시오. 
+
+
+# 2. http://127.0.0.1:8000/hello로 받은 요청을 통해 my_app 앱의 views.py에 있는 hello 함수를 실행시킬 수 있도록 아래 urls.py를 작성하시오 
+from django.urls import path
+urlpatterns = [
+    
+]
