8d3528ef6c5e9cd9c29a287a5e96d79d974f622e
Leejangha <jangha0423@gmail.com>
2023-11-07 17:58:19 +0900

    update

---
+++ Vue/23_11_07/vue_hw_4_4/my-first-front-project-mk2/src/components/MyComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <input type="text" v-model="color">
+        <p :class="color">입력창에 올바른 색상 명을 입력하면 글자색이 바껴요.</p>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const color = ref('')
+</script>
+
+<style  scoped>
+.red {
+      color: red;
+    }
+    .blue {
+      color: blue;
+    }
+    .orange {
+      color: orange;
+    }
+    .green {
+      color: green;
+    }
+    .tomato{
+        color: tomato;
+    }
+    .pdblue{
+        color: powderblue
+    }
+
+</style>
