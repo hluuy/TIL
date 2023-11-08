ff5e4ea89b90d034d942b0d075941b7bb9a0ffc1
hluuy <dudwns1289@gmail.com>
2023-11-08 22:33:30 +0900

    update

---
+++ Vue/23_11_08/vue_hw_5_4/src/components/ChildPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h3>자식 페이지입니다.</h3>
+        <p>이름 : {{ props.childInfo.name }}</p>
+        <p>나이 : {{ props.childInfo.age }}</p>
+        <p>용돈 : {{ props.childInfo.balance }}</p>
+        <button @click="buttonClick">용돈 주세요.</button>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  'childInfo': Object,
+})
+
+const emit = defineEmits(['plusMoney'])
+
+const buttonClick = function() {
+  emit('plusMoney', props.childInfo)
+}
+</script>
+
+<style scoped>
+div {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-top: 10px;
+}
+h3 {
+  font-size: 18px;
+}
+p {
+  font-size: 14px;
+}
+
+</style>
---
+++ Vue/23_11_08/vue_hw_5_4/src/components/ParentPage.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>부모 페이지 입니다.</h1>
+        <ChildPage v-for="child in children" :child-info="child" 
+        @plus-money="plusmoney"
+        />
+
+    </div>
+</template>
+
+<script setup>
+import ChildPage from "@/components/ChildPage.vue"
+import { ref } from 'vue'
+
+const children = ref([
+    {
+        name : '김하나',
+        age: 30,
+        balance: ref(100000)
+    },
+    {
+        name : '김두리',
+        age: 20,
+        balance: ref(10000)
+    },
+    {
+        name : '김서이',
+        age : 10,
+        balance: ref(1000)
+    }
+])
+const plusmoney = function(child) {
+    child.balance += 1000
+}
+
+</script>
+
+<style  scoped>
+
+</style>
